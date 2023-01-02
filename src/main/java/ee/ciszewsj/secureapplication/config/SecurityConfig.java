ae89fffa153d337ee18b54648d5220e27946bb76
jciszewski <jciszewski@amusys.pl>
2023-01-02 19:40:52 +0100

    add block

--- src/main/java/ee/ciszewsj/secureapplication/config/SecurityConfig.java
+++ src/main/java/ee/ciszewsj/secureapplication/config/SecurityConfig.java
@@ -1,17 +1,31 @@
 package ee.ciszewsj.secureapplication.config;
 
+import ee.ciszewsj.secureapplication.services.LoginAttemptService;
 import ee.ciszewsj.secureapplication.services.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.security.SecureRandom;
+import java.util.Locale;
 
 @Configuration
 @EnableWebSecurity
@@ -54,4 +68,48 @@ public class SecurityConfig {
 				.and().httpBasic();
 		return http.build();
 	}
+
+	@Component
+	public static class AuthenticationFailureListener implements
+			ApplicationListener<AuthenticationFailureBadCredentialsEvent> {
+
+		@Autowired
+		private HttpServletRequest request;
+
+		@Autowired
+		private LoginAttemptService loginAttemptService;
+
+		@Override
+		public void onApplicationEvent(AuthenticationFailureBadCredentialsEvent e) {
+			final String xfHeader = request.getHeader("X-Forwarded-For");
+			if (xfHeader == null) {
+				loginAttemptService.loginFailed(request.getRemoteAddr());
+			} else {
+				loginAttemptService.loginFailed(xfHeader.split(",")[0]);
+			}
+		}
+	}
+
+	@Component
+	public static class AuthenticationSuccessEventListener implements
+			ApplicationListener<AuthenticationSuccessEvent> {
+
+		@Autowired
+		private HttpServletRequest request;
+		
+		@Autowired
+		private LoginAttemptService loginAttemptService;
+
+		@Override
+		public void onApplicationEvent(final AuthenticationSuccessEvent e) {
+			final String xfHeader = request.getHeader("X-Forwarded-For");
+
+			if (xfHeader == null) {
+				loginAttemptService.loginSucceeded(request.getRemoteAddr());
+			} else {
+				loginAttemptService.loginSucceeded(xfHeader.split(",")[0]);
+			}
+		}
+	}
+
 }
--- src/main/java/ee/ciszewsj/secureapplication/services/UserDetailsServiceImpl.java
+++ src/main/java/ee/ciszewsj/secureapplication/services/UserDetailsServiceImpl.java
@@ -3,19 +3,37 @@ package ee.ciszewsj.secureapplication.services;
 import ee.ciszewsj.secureapplication.repository.entity.User;
 import ee.ciszewsj.secureapplication.repository.repositories.UserRepository;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
 public class UserDetailsServiceImpl implements UserDetailsService {
 	@Autowired
 	private UserRepository userRepository;
+
+	@Autowired
+	private LoginAttemptService loginAttemptService;
+
+	@Autowired
+	private HttpServletRequest request;
 
 	@SneakyThrows
 	@Override
 	public UserDetails loadUserByUsername(String username)
 			throws UsernameNotFoundException {
+
+		String ip = getClientIP();
+		log.error(ip);
+		if (loginAttemptService.isBlocked(ip)) {
+			log.error("USER IS BLOCKED");
+			throw new UsernameNotFoundException("Could not find user");
+		}
+
 		User user = userRepository.findFirstByUsernameIgnoreCase(username).orElse(null);
 		Thread.sleep(3000);
 		if (user == null) {
@@ -23,5 +41,13 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 		}
 
 		return user;
+	}
+
+	private String getClientIP() {
+		String xfHeader = request.getHeader("X-Forwarded-For");
+		if (xfHeader == null) {
+			return request.getRemoteAddr();
+		}
+		return xfHeader.split(",")[0];
 	}
 }
